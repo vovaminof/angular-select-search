3888a01e1880ba937b6a896e699e5038fcd851e5
Iskren Slavov <iskren.s@gmail.com>
2015-05-04 08:51:57 +0300

    Code style cleanup

--- dist/angular-select-search.js
+++ dist/angular-select-search.js
@@ -177,10 +177,11 @@ angular.module('selectSearch', [])
                 if (angular.isDefined(formController) && angular.isDefined(scope.name)) {
                     formController[scope.name].$touched = true;
                     formController[scope.name].$untouched = false;
-                    if(scope.ngRequired){
+                    if (scope.ngRequired) {
                         formController[scope.name].$setValidity('required',angular.isDefined(scope.value));
-                    }else{
-                        formController[scope.name].$setValidity('required',true);
+                    }
+                    else {
+                        formController[scope.name].$setValidity('required', true);
                     }
                 }
                 $animate.setClass(element, 'ng-touched', 'ng-untouched');
